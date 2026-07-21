517cdd55e2831abbddffaae8a5d000e9e214010e
SUDHANSHU KUMAR AGASTI <sudhanshuagasti588@gmail.com>
2026-07-21 18:45:53 +0530

    cretaing and als testing of username in the database and also when the user  is typing

---
+++ src/app/api/check-Username-Unique/route.ts
@@ -0,0 +1,53 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { success, z } from "zod";
+import { usernamevalidation } from "@/schemas/signUpSchema";
+
+
+
+const UsernameQuerySchma=z.object({
+    username:usernamevalidation
+})
+
+export async function GET(req:Request) {
+    await dbConnect()
+     
+    try{
+      const {searchParams}=new URL(req.url)
+      const queryParam={
+        username:searchParams.get('username')
+      }  
+      //validate with zod
+      const result= UsernameQuerySchma.safeParse(queryParam)
+      console.log(queryParam);
+
+      if(!result.success){
+        const usernameErrors=result.error.format().username?._errors||[]
+            return Response.json({
+                success:false,
+                message:"INVALID QUERY PARAMS"
+            },{status:400})
+      }
+
+      const {username}=result.data
+
+     const existingVerifiedUser = await UserModel.findOne({username,isVerified:true})
+     if(existingVerifiedUser){
+        return Response.json({
+            success:false,
+            message:"username is already taken !"
+        },{status:400})
+     }
+     return Response.json({
+        success:true,
+        message:"username should be unique !"
+     },{status:400})
+
+    }catch(error){
+        console.error("ERROR CHECKING USERNAME",error);
+        return Response.json({
+            success:false,
+            message:"Error checking username "
+        },{status:500})
+    }
+}
